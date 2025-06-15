2f0d8da3bb9d159c514db2c2828f6cf51e52fc9f
Benjamin Abendroth <braph93@gmx.de>
2025-06-15 05:06:49 +0200

    Changed "about" text

--- src/common/about.py
+++ src/common/about.py
@@ -8,6 +8,4 @@ ABOUT_NBFC_LINUX = """\
 <b>Project Homepage</b>: <a href="https://github.com/nbfc-linux/nbfc-linux">GitHub.com/NBFC-Linux/NBFC-Linux</a> <br />
 <br />
 <b>Donation Link</b>: <a href="https://paypal.me/BenjaminAbendroth">PayPal.me/BenjaminAbendroth</a> <br />
-<br />
-<b>AUR Packages</b>: Provided by <a href="https://github.com/BachoSeven">Francesco Minnocci</a>
 """
